a40889f2d97d5fc9048ab93f573722c80700a777
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 16:53:47 -0700

    Define dialogue vocalization types

--- source/units/dialogue_definitions.h
+++ source/units/dialogue_definitions.h
@@ -8,13 +8,25 @@ header included in hcex build.
 #define __DIALOGUE_DEFINITIONS_H
 #pragma once
 
+/* ---------- headers */
+
+#include "cseries/cseries.h"
+
 /* ---------- constants */
+
+enum
+{
+	NUMBER_OF_DIALOGUE_VOCALIZATION_TYPES = 209,
+};
 
 /* ---------- macros */
 
 /* ---------- structures */
 
-/* ---------- prototypes/EXAMPLE.C */
+/* ---------- prototypes/DIALOGUE_DEFINITIONS.C */
+
+char const *dialogue_get_vocalization_name(short vocalization_type, boolean abbreviated);
+short dialogue_get_vocalization_type_by_name(char const *name);
 
 /* ---------- globals */
 
