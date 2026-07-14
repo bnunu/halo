894631255fa1cb6ccc386b23c50e3534d633fe14
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 15:12:37 -0700

    Match code_001ad870

--- source/cache/predicted_resources.c
+++ source/cache/predicted_resources.c
@@ -10,6 +10,11 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "cache/predicted_resources.h"
+#include "sound/sound_definitions.h"
+#include "tag_files/tag_groups.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
@@ -18,8 +23,71 @@ symbols in this file:
 
 /* ---------- prototypes */
 
+void _sound_cache_sound_request(
+	struct sound_permutation *sound,
+	boolean load,
+	boolean reference,
+	boolean block);
+
+static void code_001ad870(long sound_definition_index);
+
 /* ---------- globals */
 
 /* ---------- public code */
 
+void predicted_resources_precache(
+	struct tag_block *predicted_resources)
+{
+	short predicted_resource_index;
+
+	for (predicted_resource_index = 0;
+		predicted_resource_index < predicted_resources->count;
+		predicted_resource_index++)
+	{
+		struct predicted_resource *predicted_resource;
+
+		predicted_resource = TAG_BLOCK_GET_ELEMENT(
+			predicted_resources,
+			predicted_resource_index,
+			struct predicted_resource);
+		if (predicted_resource->type == _predicted_resource_sound)
+			code_001ad870(predicted_resource->tag_index);
+	}
+}
+
 /* ---------- private code */
+
+static void code_001ad870(
+	long sound_definition_index)
+{
+	struct sound_definition *sound_definition;
+	struct tag_block *pitch_ranges;
+	short pitch_range_index;
+
+	sound_definition = (struct sound_definition *)tag_get(SOUND_DEFINITION_TAG, sound_definition_index);
+	pitch_range_index = 0;
+	if (sound_definition->pitch_ranges.count <= 0)
+		return;
+	pitch_ranges = &sound_definition->pitch_ranges;
+
+pitch_range_loop:
+	{
+		struct sound_pitch_range *pitch_range;
+		short permutation_index;
+
+		pitch_range = TAG_BLOCK_GET_ELEMENT(pitch_ranges, pitch_range_index, struct sound_pitch_range);
+		for (permutation_index = 0;
+			permutation_index < pitch_range->actual_permutation_count;
+			permutation_index++)
+		{
+			_sound_cache_sound_request(
+				TAG_BLOCK_GET_ELEMENT(&pitch_range->permutations, permutation_index, struct sound_permutation),
+				FALSE,
+				TRUE,
+				FALSE);
+		}
+		pitch_range_index++;
+	}
+	if (pitch_range_index < pitch_ranges->count)
+		goto pitch_range_loop;
+}
--- source/sound/sound_definitions.h
+++ source/sound/sound_definitions.h
@@ -8,6 +8,11 @@ file has inline function assertions.
 #define __SOUND_DEFINITIONS_H
 #pragma once
 
+/* ---------- headers */
+
+#include "math/real_math.h"
+#include "tag_files/tag_groups.h"
+
 /* ---------- constants */
 
 enum
@@ -37,6 +42,74 @@ enum
 /* ---------- macros */
 
 /* ---------- structures */
+
+struct real_bounds
+{
+	real lower;
+	real upper;
+};
+
+struct sound_permutation
+{
+	char name[32];
+	real skip_fraction;
+	real gain;
+	short compression;
+	short next_permutation_index;
+	long unknown0;
+	unsigned long unknown1;
+	unsigned long unknown2;
+	unsigned long sample_buffer_size;
+	unsigned long unknown3;
+	struct tag_data samples;
+	struct tag_data mouth_data;
+	struct tag_data subtitle_data;
+};
+
+struct sound_pitch_range
+{
+	char name[32];
+	real natural_pitch;
+	struct real_bounds bend_bounds;
+	short actual_permutation_count;
+	short unused;
+	real playback_rate;
+	long unknown0;
+	long unknown1;
+	struct tag_block permutations;
+};
+
+struct sound_definition
+{
+	unsigned long flags;
+	short sound_class;
+	short sample_rate;
+	real minimum_distance;
+	real maximum_distance;
+	real skip_fraction;
+	struct real_bounds random_pitch_bounds;
+	real inner_cone_angle;
+	real outer_cone_angle;
+	real outer_cone_gain;
+	real gain_modifier;
+	real maximum_bend_per_second;
+	byte unused0[12];
+	real zero_skip_fraction_modifier;
+	real zero_gain_modifier;
+	real zero_pitch_modifier;
+	byte unused1[12];
+	real one_skip_fraction_modifier;
+	real one_gain_modifier;
+	real one_pitch_modifier;
+	byte unused2[12];
+	short encoding;
+	short compression;
+	struct tag_reference promotion_sound;
+	short promotion_count;
+	short unknown0;
+	struct tag_data unknown1;
+	struct tag_block pitch_ranges;
+};
 
 /* ---------- prototypes/EXAMPLE.C */
 
