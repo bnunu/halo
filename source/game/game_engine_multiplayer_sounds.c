3e13326c4ad92dee72de469d4738cf1c340c6393
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 16:41:00 -0700

    Match multiplayer sound queue data

--- source/game/game_engine_multiplayer_sounds.c
+++ source/game/game_engine_multiplayer_sounds.c
@@ -22,15 +22,100 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+
 /* ---------- constants */
+
+enum multiplayer_information_sound
+{
+	_multiplayer_sound_play_ball,
+	_multiplayer_sound_game_over,
+	_multiplayer_sound_one_minute_to_win,
+	_multiplayer_sound_30_seconds_to_win,
+	_multiplayer_sound_red_team_minute_to_win,
+	_multiplayer_sound_red_team_30_seconds_to_win,
+	_multiplayer_sound_blue_team_minute_to_win,
+	_multiplayer_sound_blue_team_30_seconds_to_win,
+	_multiplayer_sound_blue_team_has_the_flag,
+	_multiplayer_sound_blue_team_flag_returned,
+	_multiplayer_sound_blue_team_score,
+	_multiplayer_sound_red_team_has_the_flag,
+	_multiplayer_sound_red_team_flag_returned,
+	_multiplayer_sound_red_team_score,
+	_multiplayer_sound_double_kill,
+	_multiplayer_sound_triple_kill,
+	_multiplayer_sound_killtacular,
+	_multiplayer_sound_running_riot,
+	_multiplayer_sound_killing_spree,
+	_multiplayer_sound_oddball,
+	_multiplayer_sound_race,
+	_multiplayer_sound_slayer,
+	_multiplayer_sound_capture_the_flag,
+	_multiplayer_sound_warthog,
+	_multiplayer_sound_ghost,
+	_multiplayer_sound_scorpion,
+	_multiplayer_sound_countdown_timer,
+	_multiplayer_sound_teleporter_activate,
+	_multiplayer_sound_flag_failure,
+	_multiplayer_sound_countdown_for_respawn,
+	_multiplayer_sound_hill_move,
+	_multiplayer_sound_player_respawn,
+	_multiplayer_sound_team_king_of_the_hill,
+	_multiplayer_sound_team_oddball,
+	_multiplayer_sound_team_race,
+	_multiplayer_sound_team_slayer,
+	_multiplayer_sound_king_of_the_hill,
+	_multiplayer_sound_blue_team_ctf,
+	_multiplayer_sound_red_team_ctf,
+	_multiplayer_sound_hill_contested,
+	_multiplayer_sound_hill_controlled,
+	_multiplayer_sound_hill_occupied,
+	_multiplayer_sound_countdown_timer_end,
+	_multiplayer_sound_ting,
+	NUMBER_OF_MULTIPLAYER_INFORMATION_SOUNDS
+};
+
+enum
+{
+	MAXIMUM_QUEUED_MULTIPLAYER_SOUNDS = 5,
+	MULTIPLAYER_SOUND_QUEUE_INITIAL_DELAY_TICKS = 2 * TICKS_PER_SECOND,
+};
 
 /* ---------- macros */
 
 /* ---------- structures */
 
+struct queued_multiplayer_sound
+{
+	long sound_index;
+	long delay_ticks;
+};
+
+struct multiplayer_sound_queue
+{
+	long count;
+	struct queued_multiplayer_sound sounds[MAXIMUM_QUEUED_MULTIPLAYER_SOUNDS];
+};
+
+typedef char verify_multiplayer_sound_queue_size[sizeof(struct multiplayer_sound_queue) == 0x2C ? 1 : -1];
+
 /* ---------- prototypes */
 
 /* ---------- globals */
+
+boolean data_002de530[_multiplayer_sound_ting] =
+{
+	TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+	TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+	TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+	TRUE, TRUE,
+	FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+	TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+	TRUE, TRUE, FALSE,
+};
+
+static struct multiplayer_sound_queue bss_0043eb78;
+#define multiplayer_sound_queue bss_0043eb78
 
 /* ---------- public code */
 
