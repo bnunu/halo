b25c41da14c034d6a6e6aa831c7cefb946f9295a
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 16:03:58 -0700

    Match sound_file_info_get

--- source/sound/sound_import/sound_import.c
+++ source/sound/sound_import/sound_import.c
@@ -12,6 +12,11 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "sound_import.h"
+
+#include "tag_files/files.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
@@ -20,8 +25,34 @@ symbols in this file:
 
 /* ---------- prototypes */
 
+struct sound_file_info;
+
+boolean sound_file_is_aiff(struct file_reference const *file);
+boolean sound_file_aiff_info_get(struct file_reference const *file, struct sound_file_info *info);
+
+boolean sound_file_is_wave(struct file_reference const *file);
+boolean sound_file_wave_info_get(struct file_reference const *file, struct sound_file_info *info);
+
 /* ---------- globals */
 
 /* ---------- public code */
+
+boolean sound_file_info_get(
+	struct sound_file_info *info,
+	struct file_reference const *file)
+{
+	boolean result = TRUE;
+
+	match_assert("c:\\halo\\SOURCE\\sound\\sound_import\\sound_import.c", 18, info);
+	match_assert("c:\\halo\\SOURCE\\sound\\sound_import\\sound_import.c", 19, file);
+
+	if (!(sound_file_is_aiff(file) && sound_file_aiff_info_get(file, info)) &&
+		!(sound_file_is_wave(file) && sound_file_wave_info_get(file, info)))
+	{
+		result = FALSE;
+	}
+
+	return result;
+}
 
 /* ---------- private code */
--- source/sound/sound_import/sound_import.h
+++ source/sound/sound_import/sound_import.h
@@ -14,10 +14,15 @@ header included in hcex build.
 
 /* ---------- structures */
 
+struct file_reference;
+struct sound_file_info;
+
 /* ---------- prototypes/EXAMPLE.C */
 
 /* ---------- globals */
 
 /* ---------- public code */
+
+boolean sound_file_info_get(struct sound_file_info *info, struct file_reference const *file);
 
 #endif // __SOUND_IMPORT_H
