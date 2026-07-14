1920cf32fd946cc3b3d50f283299e21b8efca235
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 12:41:00 -0700

    Match shader_type_is_valid_for_model

--- source/shaders/shaders.c
+++ source/shaders/shaders.c
@@ -153,6 +153,19 @@ boolean shader_type_is_valid_for_environment(short shader_type)
 	return result;
 }
 
+boolean shader_type_is_valid_for_model(short shader_type)
+{
+	long type = shader_type;
+	boolean result = FALSE;
+
+	if (type>=3 && type<=10)
+	{
+		result = TRUE;
+	}
+
+	return result;
+}
+
 void numeric_countdown_timer_set(long milliseconds, boolean auto_start)
 {
 	numeric_countdown_timer_milliseconds = milliseconds;
