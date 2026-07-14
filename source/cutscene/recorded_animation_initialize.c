70d98f92d1eb22b6d11958377d65b78eaa017682
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 16:27:29 -0700

    Define recorded unit control layouts

--- source/cutscene/recorded_animation_initialize.c
+++ source/cutscene/recorded_animation_initialize.c
@@ -18,15 +18,100 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "memory/byte_swapping.h"
+#include "math/real_math.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
 
 /* ---------- structures */
 
+struct recorded_unit_control
+{
+	byte control_flags;
+	byte control_mode;
+	short animation_state;
+	short weapon_index;
+	short version2_field;
+	short version3_field;
+	short pad;
+	real_vector2d throttle;
+	long reserved;
+	long version1_field;
+	real_vector3d facing;
+	real_vector3d aiming;
+	real_vector3d looking;
+};
+
+struct recorded_animation_control_field
+{
+	struct byte_swap_definition *definition;
+	long size;
+	long unit_control_offset;
+};
+
+struct recorded_animation_layout_data
+{
+	byte_swap_code real_vector2d_codes[2];
+	struct byte_swap_definition real_vector2d_definition;
+	byte_swap_code real_vector3d_codes[3];
+	struct byte_swap_definition real_vector3d_definition;
+	long version_table_alignment;
+	struct recorded_animation_control_field version0_fields[10];
+	struct recorded_animation_control_field version1_fields[2];
+	struct recorded_animation_control_field version2_fields[2];
+	struct recorded_animation_control_field version3_fields[2];
+	struct recorded_animation_control_field *fields_by_version[4];
+};
+
 /* ---------- prototypes */
 
+extern struct byte_swap_definition byte_bs_definition;
+extern struct byte_swap_definition word_bs_definition;
+extern struct byte_swap_definition long_bs_definition;
+
 /* ---------- globals */
+
+struct recorded_animation_layout_data data_002dce10 =
+{
+	{ _4byte, _4byte },
+	{ "real_vector2d", sizeof(real_vector2d), data_002dce10.real_vector2d_codes, BYTE_SWAP_DEFINITION_SIGNATURE, FALSE },
+	{ _4byte, _4byte, _4byte },
+	{ "real_vector3d", sizeof(real_vector3d), data_002dce10.real_vector3d_codes, BYTE_SWAP_DEFINITION_SIGNATURE, FALSE },
+	0, /* the original independently declared version table begins on an 8-byte data boundary */
+	{
+		{ &byte_bs_definition, sizeof(byte), offsetof(struct recorded_unit_control, control_flags) },
+		{ &byte_bs_definition, sizeof(byte), offsetof(struct recorded_unit_control, control_mode) },
+		{ &word_bs_definition, sizeof(short), offsetof(struct recorded_unit_control, animation_state) },
+		{ &word_bs_definition, sizeof(short), offsetof(struct recorded_unit_control, weapon_index) },
+		{ &word_bs_definition, sizeof(short), NONE },
+		{ &data_002dce10.real_vector2d_definition, sizeof(real_vector2d), offsetof(struct recorded_unit_control, throttle) },
+		{ &data_002dce10.real_vector3d_definition, sizeof(real_vector3d), offsetof(struct recorded_unit_control, facing) },
+		{ &data_002dce10.real_vector3d_definition, sizeof(real_vector3d), offsetof(struct recorded_unit_control, aiming) },
+		{ &data_002dce10.real_vector3d_definition, sizeof(real_vector3d), offsetof(struct recorded_unit_control, looking) },
+		{ NULL, NONE, NONE },
+	},
+	{
+		{ &long_bs_definition, sizeof(long), offsetof(struct recorded_unit_control, version1_field) },
+		{ NULL, NONE, NONE },
+	},
+	{
+		{ &word_bs_definition, sizeof(short), offsetof(struct recorded_unit_control, version2_field) },
+		{ NULL, NONE, NONE },
+	},
+	{
+		{ &word_bs_definition, sizeof(short), offsetof(struct recorded_unit_control, version3_field) },
+		{ NULL, NONE, NONE },
+	},
+	{
+		data_002dce10.version0_fields,
+		data_002dce10.version1_fields,
+		data_002dce10.version2_fields,
+		data_002dce10.version3_fields,
+	},
+};
 
 /* ---------- public code */
 
