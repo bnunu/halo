4255cd821fa22e08563eb97f55a24374b6105263
Jonas Volman <theunknowentity@gmail.com>
2026-07-14 15:45:50 -0700

    Match render media level-of-detail data

--- source/render/render_media.c
+++ source/render/render_media.c
@@ -22,6 +22,7 @@ symbols in this file:
 /* ---------- headers */
 
 #include "cseries.h"
+#include "real_math.h"
 
 /* ---------- constants */
 
@@ -37,11 +38,52 @@ struct render_media_globals
 	byte runtime_state[0x24];
 };
 
+struct frame_level_of_detail
+{
+	long level_count;
+	real base_fraction;
+	real frame_fraction_thresholds[5];
+	real reserved_thresholds[3];
+	real detail_fraction_thresholds[3];
+	real distance_bias;
+	real distance_thresholds[5];
+	real maximum_level;
+	real minimum_level;
+};
+
 /* ---------- prototypes */
 
 /* ---------- globals */
 
 static struct render_media_globals render_media_globals;
+
+const real media_wave_amplitude = 0.05f;
+
+__declspec(align(8)) struct frame_level_of_detail standard_frame_level_of_detail =
+{
+	4,
+	0.0f,
+	{ 0.33f, 0.66f, 1.0f, 0.0f, 0.0f },
+	{ 0.0f, 0.0f, 0.0f },
+	{ 0.0f, 0.0f, 0.0f },
+	0.0f,
+	{ 200.0f, 400.0f, 700.0f, 0.0f, 0.0f },
+	3.0f,
+	2.0f
+};
+
+__declspec(align(8)) struct frame_level_of_detail render_target_frame_level_of_detail =
+{
+	6,
+	0.0f,
+	{ 0.2f, 0.4f, 0.6f, 0.8f, 1.0f },
+	{ 0.0f, 0.0f, 0.0f },
+	{ 0.4f, 0.7f, 1.0f },
+	0.0f,
+	{ 50.0f, 70.0f, 125.0f, 250.0f, 500.0f },
+	6.0f,
+	4.0f
+};
 
 /* ---------- public code */
 
