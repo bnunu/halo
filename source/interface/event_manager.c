f31445792278e32d66173d7cab0675529b6b0895
Jonas Volman <theunknowentity@gmail.com>
2026-07-16 08:34:01 -0700

    Match event manager queue leaves

--- source/interface/event_manager.c
+++ source/interface/event_manager.c
@@ -28,16 +28,122 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "cseries_windows.h"
+#include "event_manager.h"
+#include "input.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
 
 /* ---------- structures */
 
+struct event_manager_state
+{
+	boolean initialized;
+	boolean suppressed;
+	short __unknown2;
+	unsigned long time_of_last_event;
+	struct event_record events[MAXIMUM_GAMEPADS][8];
+};
+
+struct event_manager_globals
+{
+	struct event_manager_state state;
+	byte __unknown108[0x60];
+};
+
+typedef char verify_event_manager_state_size[
+	sizeof(struct event_manager_state) == 0x108 ? 1 : -1];
+typedef char verify_event_manager_globals_size[
+	sizeof(struct event_manager_globals) == 0x168 ? 1 : -1];
+
 /* ---------- prototypes */
 
 /* ---------- globals */
 
+struct event_manager_globals event_manager_globals;
+
 /* ---------- public code */
+
+void event_manager_initialize(
+	void)
+{
+	csmemset(&event_manager_globals.state, 0, sizeof(event_manager_globals.state));
+	event_manager_globals.state.time_of_last_event = system_milliseconds();
+	event_manager_globals.state.initialized = TRUE;
+	return;
+}
+
+void event_manager_dispose(
+	void)
+{
+	csmemset(&event_manager_globals.state, 0, sizeof(event_manager_globals.state));
+	return;
+}
+
+void event_manager_flush(
+	void)
+{
+	csmemset(event_manager_globals.state.events, 0, sizeof(event_manager_globals.state.events));
+	return;
+}
+
+void event_manager_suppress(
+	boolean suppress)
+{
+	event_manager_globals.state.suppressed = suppress;
+	return;
+}
+
+boolean get_next_event(
+	struct event_record *event,
+	short local_player_index)
+{
+	boolean result = FALSE;
+
+	match_assert(
+		"c:\\halo\\SOURCE\\interface\\event_manager.c",
+		211,
+		event && ((local_player_index>=0 && local_player_index<MAXIMUM_GAMEPADS) || local_player_index==NONE));
+
+	if (event_manager_globals.state.initialized)
+	{
+		if (local_player_index == NONE)
+		{
+			local_player_index = 0;
+			while (!result)
+			{
+				if (local_player_index >= MAXIMUM_GAMEPADS)
+					break;
+				result = get_next_event(event, local_player_index);
+				local_player_index++;
+			}
+		}
+		else
+		{
+			long event_index;
+
+			for (event_index = 7; event_index >= 0; event_index--)
+			{
+				if (event_manager_globals.state.events[local_player_index][event_index].type != 0)
+				{
+					*event = event_manager_globals.state.events[local_player_index][event_index];
+					event_manager_globals.state.events[local_player_index][event_index].type = 0;
+					return TRUE;
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
+unsigned long event_manager_time_of_last_event(
+	void)
+{
+	return event_manager_globals.state.time_of_last_event;
+}
 
 /* ---------- private code */
--- source/interface/event_manager.h
+++ source/interface/event_manager.h
@@ -14,14 +14,36 @@ header included in hcex build.
 
 /* ---------- structures */
 
+struct event_record
+{
+	short type;
+	short __unknown2;
+	long data;
+};
+
 /* ---------- prototypes/EVENT_MANAGER.C */
 
-unsigned long event_manager_time_of_last_event(void);
+void event_manager_initialize(
+	void);
+
+void event_manager_dispose(
+	void);
+
+void event_manager_suppress(
+	boolean suppress);
+
+boolean get_next_event(
+	struct event_record *event,
+	short local_player_index);
+
+unsigned long event_manager_time_of_last_event(
+	void);
 
 void event_manager_flush(
 	void);
 
-void event_manager_update(void);
+void event_manager_update(
+	void);
 
 /* ---------- globals */
 
