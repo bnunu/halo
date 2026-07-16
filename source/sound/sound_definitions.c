309b63364999b378ac2d504f393659975957f8fa
Jonas Volman <theunknowentity@gmail.com>
2026-07-16 01:00:08 -0700

    Match sound definition distance and mouth helpers

--- source/sound/sound_definitions.c
+++ source/sound/sound_definitions.c
@@ -31,16 +31,111 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "errors.h"
+#include "sound_definitions.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
 
 /* ---------- structures */
 
+struct sound_class_definition
+{
+	byte unused[0x18];
+	real minimum_distance;
+	real maximum_distance;
+};
+
 /* ---------- prototypes */
+
+struct sound_class_definition *sound_class_get(
+	short sound_class);
 
 /* ---------- globals */
 
+unsigned long const sound_sample_rate_samples_per_second[2] =
+{
+	22050,
+	44100
+};
+
+real const oo_unsigned_char_max = 1.f / 255.f;
+
 /* ---------- public code */
+
+real sound_definition_get_maximum_distance(
+	long sound_index)
+{
+	struct sound_definition *definition = sound_definition_get(sound_index);
+	real maximum_distance = definition->maximum_distance;
+
+	if (maximum_distance == 0.f)
+		maximum_distance = sound_class_get(definition->sound_class)->maximum_distance;
+
+	return maximum_distance;
+}
+
+real sound_definition_get_minimum_distance(
+	long sound_index)
+{
+	struct sound_definition *definition = sound_definition_get(sound_index);
+	real minimum_distance = definition->minimum_distance;
+
+	if (minimum_distance == 0.f)
+		minimum_distance = sound_class_get(definition->sound_class)->minimum_distance;
+
+	return minimum_distance;
+}
+
+byte *sound_permutation_get_mouth_aperture(
+	struct sound_permutation *permutation,
+	short tick_index)
+{
+	match_assert(
+		"c:\\halo\\SOURCE\\sound\\sound_definitions.c",
+		800,
+		tick_index>=0 && tick_index<permutation->mouth_data.size);
+
+	return (byte *)permutation->mouth_data.address + tick_index;
+}
+
+void try_to_reset_permutations(
+	struct sound_pitch_range *range)
+{
+	short permutation_count = range->actual_permutation_count;
+	unsigned long all_permutations_mask = (FLAG(permutation_count) - 1);
+
+	if ((~range->unknown0 & all_permutations_mask) == 0)
+	{
+		range->unknown0 = 0;
+		if (permutation_count > 1)
+			range->unknown0 = FLAG((byte)range->unknown1);
+	}
+
+	return;
+}
+
+real sound_permutation_get_real_mouth_aperture(
+	struct sound_permutation *permutation,
+	long tick_index)
+{
+	if (permutation->mouth_data.size)
+	{
+		long aperture = *sound_permutation_get_mouth_aperture(
+			permutation,
+			PIN((short)tick_index, 0, permutation->mouth_data.size - 1));
+
+		return aperture * oo_unsigned_char_max;
+	}
+
+	error(
+		_error_silent,
+		"but how can you speak if you have no mouth data? (permutation %s)",
+		permutation->name);
+
+	return 0.f;
+}
 
 /* ---------- private code */
