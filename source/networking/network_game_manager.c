9e9cce3fda16566d3c96f81811694d56c63d0f34
Jonas Volman <theunknowentity@gmail.com>
2026-07-16 23:03:19 -0700

    Match network game manager helpers

--- source/networking/network_game_manager.c
+++ source/networking/network_game_manager.c
@@ -77,18 +77,41 @@ symbols in this file:
 /* ---------- headers */
 
 #include "cseries/cseries.h"
+#include "game/game.h"
 #include "game/players.h"
+#include "main/main.h"
+#include "network_game_globals.h"
 #include "network_game_manager.h"
+#include "text/unicode.h"
 
 /* ---------- constants */
 
 /* ---------- macros */
 
+#define network_machine_is_valid(machine) \
+	((machine) && (machine)->machine_index >= 0 && (machine)->machine_index < 4)
+
 /* ---------- structures */
+
+struct network_machine
+{
+	byte __unknown0[0x40];
+	char machine_index;
+	byte __padding41[3];
+};
 
 struct network_game
 {
-	byte __unknown0[0x430];
+	byte __unknown0[0x10D];
+	byte game_mode;
+	byte maximum_player_count;
+	byte __padding10F;
+	short difficulty;
+	short machine_count;
+	struct network_machine machines[4];
+	short player_count;
+	struct network_player players[16];
+	byte __unknown426[0xA];
 	boolean load_ui;
 	byte __padding431[3];
 };
@@ -97,10 +120,99 @@ struct network_game
 
 void main_load_ui_scenario(
 	boolean load_ui);
+long __stdcall XFindFirstNicknameW(
+	long this_title_only,
+	wchar_t *nickname,
+	unsigned long size);
+long __stdcall XFindClose(
+	long handle);
+long __stdcall XSetNicknameW(
+	wchar_t const *nickname,
+	long preserve_case);
+wchar_t const *network_game_get_random_player_name(
+	void);
+boolean network_game_player_is_local(
+	struct network_player const *player);
+long player_new(
+	long machine_index,
+	long unit_index,
+	short controller_index,
+	struct network_player const *player);
 
 /* ---------- globals */
 
 /* ---------- public code */
+
+boolean network_game_update_machine(
+	struct network_game *game,
+	struct network_machine *machine)
+{
+	long machine_index;
+	boolean result = FALSE;
+
+	match_assert(
+		"c:\\halo\\SOURCE\\networking\\network_game_manager.c",
+		0x81,
+		game && machine && network_machine_is_valid(machine));
+
+	for (machine_index = 0; machine_index < 4; machine_index++)
+	{
+		if (game->machines[machine_index].machine_index == machine->machine_index)
+		{
+			csmemcpy(&game->machines[machine_index], machine, sizeof(*machine));
+			result = TRUE;
+			break;
+		}
+	}
+
+	return result;
+}
+
+void xbox_set_machine_name(
+	char const *machine_name)
+{
+	wchar_t wide_machine_name[32];
+
+	if (machine_name && machine_name[0])
+	{
+		if (ascii_to_wide(machine_name, wide_machine_name, sizeof(wide_machine_name)))
+		{
+			wide_machine_name[31] = 0;
+			if (!XSetNicknameW(wide_machine_name, TRUE))
+				error(2, "XSetNickname() failed");
+		}
+		else
+		{
+			error(2, "'%s' is not a valid machine name (max. name length= %d characters)", wide_machine_name, 31);
+		}
+	}
+
+	return;
+}
+
+void network_game_generate_local_machine_name(
+	wchar_t *machine_name)
+{
+	char ascii_machine_name[32];
+	long find_handle = XFindFirstNicknameW(FALSE, machine_name, 32);
+
+	if (find_handle == NONE)
+	{
+		ustrncpy(machine_name, network_game_get_random_player_name(), 32);
+		machine_name[31] = 0;
+		if (XSetNicknameW(machine_name, TRUE))
+			error(2, "system nickname set to '%s'", wide_to_ascii(machine_name, ascii_machine_name, 32));
+		else
+			error(2, "XSetNickname() failed to set system nickname");
+	}
+	else
+	{
+		XFindClose(find_handle);
+	}
+
+	machine_name[31] = 0;
+	return;
+}
 
 void network_game_invalidate_player(
 	struct network_player *player)
@@ -149,6 +261,85 @@ boolean network_player_is_valid(
 	}
 
 	return FALSE;
+}
+
+boolean network_game_spawn_player(
+	struct network_player *player)
+{
+	long player_index;
+	short controller_index;
+
+	match_assert(
+		"c:\\halo\\SOURCE\\networking\\network_game_manager.c",
+		0x1BC,
+		network_player_is_valid(player));
+
+	controller_index = network_game_player_is_local(player) ? player->controller_index : NONE;
+	player_index = player_new(player->machine_index, NONE, controller_index, player);
+	if (player_index != NONE)
+	{
+		player->player_list_index = (char)player_index;
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+boolean network_game_player_is_valid(
+	struct network_player *player,
+	struct network_game *game)
+{
+	long player_index;
+	struct network_player *current_player;
+	boolean result = FALSE;
+
+	match_assert(
+		"c:\\halo\\SOURCE\\networking\\network_game_manager.c",
+		0x247,
+		player && game);
+
+	if (network_player_is_valid(player))
+	{
+		current_player = &game->players[0];
+		for (player_index = 0; player_index < 16; player_index++, current_player++)
+		{
+			if (current_player->machine_index == player->machine_index &&
+				current_player->controller_index == player->controller_index)
+			{
+				result = TRUE;
+				break;
+			}
+		}
+	}
+
+	return result;
+}
+
+void network_game_reset_for_next_round(
+	struct network_game *game,
+	boolean load_ui)
+{
+	match_assert(
+		"c:\\halo\\SOURCE\\networking\\network_game_manager.c",
+		0x22D,
+		game);
+
+	if (load_ui && game->load_ui)
+	{
+		main_load_ui_scenario(TRUE);
+		csmemset(&game->load_ui, 0, sizeof(long));
+		if (global_network_game_server_get())
+			game_connection_set(2);
+		else if (global_network_game_client_get())
+			game_connection_set(1);
+	}
+	else
+	{
+		csmemset(&game->load_ui, 0, sizeof(long));
+	}
+
+	game_time_end();
+	return;
 }
 
 /* ---------- private code */
--- source/networking/network_game_manager.h
+++ source/networking/network_game_manager.h
@@ -15,6 +15,7 @@ header included in hcex build.
 /* ---------- structures */
 
 struct network_game;
+struct network_machine;
 struct network_player;
 
 /* ---------- prototypes/EXAMPLE.C */
@@ -27,6 +28,21 @@ void code_0011a4c0(
 	void);
 boolean network_player_is_valid(
 	struct network_player *player);
+boolean network_game_update_machine(
+	struct network_game *game,
+	struct network_machine *machine);
+void xbox_set_machine_name(
+	char const *machine_name);
+void network_game_generate_local_machine_name(
+	wchar_t *machine_name);
+boolean network_game_spawn_player(
+	struct network_player *player);
+boolean network_game_player_is_valid(
+	struct network_player *player,
+	struct network_game *game);
+void network_game_reset_for_next_round(
+	struct network_game *game,
+	boolean load_ui);
 
 /* ---------- globals */
 
