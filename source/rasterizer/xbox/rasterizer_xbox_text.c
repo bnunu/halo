76584d8d62fb28c030203e2f6d95147e6fd3b1a6
Jonas Volman <theunknowentity@gmail.com>
2026-07-18 10:11:50 -0700

    Match rasterizer text character drawing

--- source/rasterizer/xbox/rasterizer_xbox_text.c
+++ source/rasterizer/xbox/rasterizer_xbox_text.c
@@ -40,21 +40,153 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "cseries/errors.h"
+#include "real_math.h"
+/* The January object retains out-of-line copies of the D3D inline wrappers:
+ * code_00162ea0 = D3DDevice_SetRenderState
+ * code_00163050 = D3DDevice_SetTextureStageState
+ * code_001630a0 = IDirect3DDevice8_SetRenderState
+ * code_001632c0 = IDirect3DDevice8_SetTextureStageState
+ * code_00163320 = IDirect3DDevice8_SetVertexShaderConstant
+ * code_00163340 = IDirect3DDevice8_SetVertexData2f
+ * code_00163360 = IDirect3DDevice8_SetVertexDataColor
+ * code_00163370 = IDirect3DDevice8_Begin
+ * code_00163380 = IDirect3DDevice8_End
+ */
+#define D3DINLINE static
+#include <xtl.h>
+
 /* ---------- constants */
 
 /* ---------- macros */
 
 /* ---------- structures */
 
+struct rasterizer_text_vertex
+{
+	real x;
+	real y;
+	real u;
+	real v;
+	unsigned long color;
+};
+
+struct rasterizer_text_debug_options
+{
+	byte reserved00[0x22];
+	boolean render_text;
+};
+
+struct rasterizer_text_window_parameters
+{
+	short rasterizer_target;
+};
+
 /* ---------- prototypes */
 
+void rasterizer_error(
+	long error_result,
+	char const *format,
+	...);
+
 /* ---------- globals */
+
+extern void *global_d3d_device;
+extern struct rasterizer_text_debug_options rasterizer_debug_options;
+extern struct rasterizer_text_window_parameters global_window_parameters;
 
 /* ---------- public code */
 
 void rasterizer_text_end(
 	void)
 {
+	return;
+}
+
+void rasterizer_text_draw_character(
+	struct rasterizer_text_vertex const *vertices)
+{
+	boolean success;
+	short vertex_index;
+
+	match_assert(
+		"c:\\halo\\SOURCE\\rasterizer\\xbox\\rasterizer_xbox_text.c",
+		216,
+		global_d3d_device);
+
+	if (rasterizer_debug_options.render_text &&
+		global_window_parameters.rasterizer_target == 0)
+	{
+		success = IDirect3DDevice8_Begin(
+			global_d3d_device,
+			D3DPT_TRIANGLEFAN) >= 0;
+		for (vertex_index = 0; vertex_index < 4; vertex_index++)
+		{
+			if (IDirect3DDevice8_SetVertexDataColor(
+				global_d3d_device,
+				9,
+				vertices[vertex_index].color) >= 0 && success)
+			{
+				success = TRUE;
+			}
+			else
+			{
+				success = FALSE;
+				rasterizer_error(
+					0,
+					"IDirect3DDevice8_SetVertexDataColor(global_d3d_device, 9, vertices[vertex_index].color)");
+			}
+
+			if (IDirect3DDevice8_SetVertexData2f(
+				global_d3d_device,
+				4,
+				vertices[vertex_index].u,
+				vertices[vertex_index].v) >= 0 && success)
+			{
+				success = TRUE;
+			}
+			else
+			{
+				success = FALSE;
+				rasterizer_error(
+					0,
+					"IDirect3DDevice8_SetVertexData2f(global_d3d_device, 4, vertices[vertex_index].texcoord.u, vertices[vertex_index].texcoord.v)");
+			}
+
+			if (IDirect3DDevice8_SetVertexData2f(
+				global_d3d_device,
+				0,
+				vertices[vertex_index].x,
+				vertices[vertex_index].y) >= 0 && success)
+			{
+				success = TRUE;
+			}
+			else
+			{
+				success = FALSE;
+				rasterizer_error(
+					0,
+					"IDirect3DDevice8_SetVertexData2f(global_d3d_device, VSDE_VERTEX, vertices[vertex_index].position.x, vertices[vertex_index].position.y)");
+			}
+		}
+
+		if (IDirect3DDevice8_End(global_d3d_device) >= 0 && success)
+		{
+			success = TRUE;
+		}
+		else
+		{
+			success = FALSE;
+			rasterizer_error(
+				0,
+				"IDirect3DDevice8_End(global_d3d_device)");
+		}
+
+		if (!success)
+			error(2, "### ERROR rasterizer_text_draw_character failed");
+	}
+
 	return;
 }
 
