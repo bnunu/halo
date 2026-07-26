6fdda8b2c3c5c6c31335b72cc00773969fdcc0fb
Jonas Volman <theunknowentity@gmail.com>
2026-07-25 23:03:27 -0700

    Match elimination game variant builder

--- source/game/game_engine.c
+++ source/game/game_engine.c
@@ -2310,6 +2310,34 @@ struct game_variant *build_game_variant_team_slayer(
 	return variant;
 }
 
+struct game_variant *build_game_variant_elimination(
+	struct game_variant *variant)
+{
+	struct game_variant result = { 0 };
+
+	result.flags = (result.flags & ~0x3C) | 3;
+	result.engine_type = 2;
+	result.maximum_lives = 1;
+	result.unknown48 = 2;
+	result.unknown64 = 1;
+	result.unknown24 = 0;
+	result.unknown3C = 1.0f;
+	result.unknown28 = FALSE;
+	result.unknown30 = 0;
+	result.unknown2C = 0;
+	result.unknown40 = 25;
+	result.unknown34 = 300;
+	result.has_teams = FALSE;
+	result.unknown44 = 0;
+	result.unknown4C.byte0 = 0;
+	result.unknown4C.byte1 = 0;
+	result.unknown4C.byte2 = 0;
+
+	*variant = result;
+
+	return variant;
+}
+
 long postgame_statistic_get_rating(
 	long player_index,
 	long parameter1,
