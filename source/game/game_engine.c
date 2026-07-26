089ff2dcd09b0ccc0f72ec3f19d8c9d7c8cb74ff
Jonas Volman <theunknowentity@gmail.com>
2026-07-25 23:02:11 -0700

    Match team slayer game variant builder

--- source/game/game_engine.c
+++ source/game/game_engine.c
@@ -2282,6 +2282,34 @@ struct game_variant *build_game_variant_slayer(
 	return variant;
 }
 
+struct game_variant *build_game_variant_team_slayer(
+	struct game_variant *variant)
+{
+	struct game_variant result = { 0 };
+
+	result.flags = (result.flags & ~0x3C) | 3;
+	result.unknown30 = 300;
+	result.unknown34 = 300;
+	result.engine_type = 2;
+	result.has_teams = TRUE;
+	result.unknown48 = 2;
+	result.unknown64 = 1;
+	result.unknown24 = 0;
+	result.unknown3C = 1.0f;
+	result.maximum_lives = 0;
+	result.unknown28 = FALSE;
+	result.unknown2C = 0;
+	result.unknown40 = 50;
+	result.unknown44 = 0;
+	result.unknown4C.byte0 = 0;
+	result.unknown4C.byte1 = 0;
+	result.unknown4C.byte2 = 0;
+
+	*variant = result;
+
+	return variant;
+}
+
 long postgame_statistic_get_rating(
 	long player_index,
 	long parameter1,
