4ddde32deb66e11d2815690c349db704b38d529c
Jonas Volman <theunknowentity@gmail.com>
2026-07-25 23:34:29 -0700

    Match accumulation game variant builder

--- source/game/game_engine.c
+++ source/game/game_engine.c
@@ -2546,6 +2546,37 @@ struct game_variant *build_game_variant_reverse_tag(
 	return variant;
 }
 
+struct game_variant *build_game_variant_accumulation(
+	struct game_variant *variant)
+{
+	struct game_variant result = { 0 };
+
+	result.flags = (result.flags & ~0x3D) | 2;
+	result.unknown30 = 150;
+	result.unknown34 = 150;
+	result.unknown48 = 1;
+	result.unknown5C = 1;
+	result.unknown64 = 1;
+	result.engine_type = 3;
+	result.unknown24 = 2;
+	result.unknown3C = 1.0f;
+	result.maximum_lives = 0;
+	result.unknown28 = FALSE;
+	result.unknown2C = 0;
+	result.unknown40 = 5;
+	result.has_teams = FALSE;
+	result.unknown44 = 0;
+	result.unknown60 = 16;
+	result.unknown4C.byte1 = 0;
+	result.unknown4C.byte0 = 0;
+	result.unknown54 = 0;
+	result.unknown58 = 0;
+
+	*variant = result;
+
+	return variant;
+}
+
 struct game_variant *build_game_variant_king_pro(
 	struct game_variant *variant)
 {
