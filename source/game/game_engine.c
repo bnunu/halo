08828e674d65a511578580fa93aa5d3d5e0fe322
Jonas Volman <theunknowentity@gmail.com>
2026-07-25 23:06:32 -0700

    Match endurance game variant builder

--- source/game/game_engine.c
+++ source/game/game_engine.c
@@ -2366,6 +2366,34 @@ struct game_variant *build_game_variant_phantoms(
 	return variant;
 }
 
+struct game_variant *build_game_variant_endurance(
+	struct game_variant *variant)
+{
+	struct game_variant result = { 0 };
+
+	result.flags = (result.flags & ~0x3C) | 3;
+	result.engine_type = 2;
+	result.unknown28 = TRUE;
+	result.unknown2C = 300;
+	result.unknown48 = 2;
+	result.unknown64 = 1;
+	result.unknown34 = 300;
+	result.unknown24 = 0;
+	result.unknown3C = 1.0f;
+	result.maximum_lives = 5;
+	result.unknown30 = 0;
+	result.unknown40 = 10;
+	result.has_teams = FALSE;
+	result.unknown44 = 0;
+	result.unknown4C.byte0 = 0;
+	result.unknown4C.byte1 = 0;
+	result.unknown4C.byte2 = 0;
+
+	*variant = result;
+
+	return variant;
+}
+
 long postgame_statistic_get_rating(
 	long player_index,
 	long parameter1,
